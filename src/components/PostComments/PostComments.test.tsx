db1d0e7a9b70ff0bd3d4d49ce13c3e6a2da6cac9
Octávio Fernandes <65549635+Tavimfernandes@users.noreply.github.com>
2024-02-13 18:03:15 -0300

    exercicio ebac

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,32 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
+// import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
     it('Deve renderizar o componente corretamente', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
-    });
-});
+    })
+
+    test('Deve adcionar dois comentarios', () => {
+        render(<PostComment />)
+
+         // Primeiro comentário
+        fireEvent.change(screen.getByTestId('campoComentario'), {
+            target: {
+                value: 'Primeiro comentario de teste'
+            }
+        })
+        fireEvent.click(screen.getByTestId('btn-comentar'))
+
+        // Segundo comentário
+        fireEvent.change(screen.getByTestId('campoComentario'), {
+            target: {
+                value: 'Segundo comentario de teste'
+            }
+        })
+        fireEvent.click(screen.getByTestId('btn-comentar'))
+
+        expect(screen.getAllByTestId('comentarioTeste')).toHaveLength(2)
+    })
+})
